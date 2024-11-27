e17f738822dccbb9c31c4c9d1ee5da3b61256bb5
DebasishWebKorps <debasish@webkorps.com>
2024-11-28 03:34:43 +0530

    initialize

---
+++ cicd_demo/client/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h2>User List</h2> 
+     <h4>User1</h4>
+     <h4>User2</h4>
+     <h4>User3</h4>
+     <p>Thank you</p>
+     <p>Git testing rebase and merge</p>
+     </>
+  )
+}
+
+export default App
